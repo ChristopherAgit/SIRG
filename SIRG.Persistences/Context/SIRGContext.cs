2c3846a483a67e4d73cbaace0a56613a49ebe07e
Christopher Agramonte Martinez <20240150@itla.edu.do>
2026-02-04 15:00:14 -0400

    Repository

---
+++ SIRG.Persistences/Context/SIRGContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SIRG.Persistences.Context
+{
+    public class SIRGContext : DbContext
+    {
+        public SIRGContext(DbContextOptions<SIRGContext> options) : base(options) 
+        {
+        
+        }
+        
+         
+
+    }
+}
